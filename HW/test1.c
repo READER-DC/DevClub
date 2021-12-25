fe2a3f3710f59bbe5ea6dd2f83132251231e08b2
Anatolii Sehal <sab198005@gmail.com>
2021-12-25 23:06:47 +0200

    save change files

--- HW/test1.c
+++ HW/test1.c
@@ -1,51 +1,41 @@
 /*
-void matrixCopy(int target[SIZE][SIZE], int source[SIZE][SIZE], int size);
+Задача matrixRotate90 
+void matrixRotate90(int target[SIZE][SIZE], int source[SIZE][SIZE], int size);
+
 */
-
-
 
 #include <stdio.h>
 #define SIZE 4
-void matrixCopy(int target[SIZE][SIZE], int source[SIZE][SIZE], int size);{
-    for ( int i = 0; i < size; i++ ) {
-        for (int j = 0; j < size; j++) {
-            target[i][j] = source[i][j];
-        }
-    }
-}
 
-void matrixMultiply(int matrix[SIZE][SIZE], int size, int multiplier) {
-    for( int row = 0; row < size; row++ ) {
-        for( int col = 0; col < size; col++ ){
-            matrix[row][col] *= multiplier;
-        }
-    }
-}
-
-void matrixFiell (int matrix[][SIZE]) {
-    for ( int row = 0; row < SIZE; row++) {
-        for ( int col = 0; col < SIZE; col++) {
-            matrix[row][col] = row*col + (col);
-        }
-    }
-}
-
-void matrixPrint(int matrix[][SIZE]) {
-    for ( int row = 0; row < SIZE; row++) {
-        for ( int col = 0; col < SIZE-1; col++) {
-            printf("%d ", matrix[row][col] );
-        }
-        printf("%d\n", matrix[row][SIZE-1] );
-    }
-}
-
-void matrixIncrement(int matrix[SIZE][SIZE], int increment[SIZE][SIZE], int size) {
+void matrixRotate90(int target[SIZE][SIZE], int source[SIZE][SIZE], int size) {
     for ( int row = 0; row < size; row++ ) {
         for ( int col = 0; col < size; col++ ) {
-            matrix[row][col] = matrix[row][col] + increment[row][col];
+            int targetCol = size - row - 1;
+            
+            target[row][col] = source[col][targetCol];
         }
     }
 }
+
+
+// void matrixCopy(int target[SIZE][SIZE], int source[SIZE][SIZE], int size) {
+//     for ( int i = 0; i < size; i++ ) {
+//         for (int j = 0; j < size; j++) {
+//             target[i][j] = source[i][j];
+
+//         }
+//     }
+// }
+
+// void matrixPrint(int matrix[SIZE][SIZE]) {
+//     for ( int row = 0; row < SIZE; row++) {
+//         for ( int col = 0; col < SIZE-1; col++) {
+//             printf("%d ", matrix[row][col] );
+//         }
+//         printf("%d\n", matrix[row][SIZE-1] );
+//     }
+// }
+
 void matrixScan(FILE *in, int matrix[SIZE][SIZE], int size) {
     for ( int row = 0; row < size; row++ ) {
         for ( int col = 0; col < size; col++ ) {
@@ -58,7 +48,7 @@ void matrixPrint(FILE *out, int matrix[SIZE][SIZE], int size) {
     
     for ( int row = 0; row < size; row++ ) {
         for ( int col = 0; col < last; col++ ) {
-            fprintf(out, "%d ", matrix[col][row]);
+            fprintf(out, "%d_", matrix[col][row]);
         }
         fprintf(out, "%d\n", matrix[last][row]);
     }
@@ -72,9 +62,10 @@ int main() {
     int matrix[SIZE][SIZE];
     int decrement[SIZE][SIZE];
     matrixScan(in,matrix, 4);
-    matrixMultiply(matrix, 4, 10);
-    matrixPrint(out, matrix, 4);
-    // matrixPrint(matrix);
+    matrixRotate90(decrement, matrix, 4);
+
+    matrixPrint(out, decrement, 4);
+    // matrixPrint(decrement);
 
     
     // matrixFiell(matrix);
@@ -88,23 +79,3 @@ int main() {
 
     return 0;
 }
-
-/* 1/4 tests passed
-void matrixScan(FILE *in, int matrix[SIZE][SIZE], int size) {
-    for ( int i = 0; i < size; i++ ) {
-        for ( int j = 0; j < size; j++ ) {
-            fscanf(in, "%d", &matrix[i][j]);
-        }
-    }
-}
-*/
-
-/*
-
-/*  1/4 tests passed
-void matrixScan(FILE *in, int matrix[SIZE][SIZE], int size) {
-    for ( int i = 0; i < size; i++ ) {
-        for ( int j = 0; j < size && fscanf(in, "%d", &matrix[i][j]) == 1; j++ );
-    }
-}
-*/

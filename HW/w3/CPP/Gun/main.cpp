0c7d2a9208c09f7a14a4975f92595978f6c6feec
ANATOLII <sab198005@gmail.com>
2022-02-06 15:47:40 +0200

    UPD gun.cpp is redy

---
+++ HW/w3/CPP/Gun/main.cpp
@@ -0,0 +1,19 @@
+#include "gun.h"
+
+int main() {
+    
+    Gun gun;
+    Gun beretta("Beretta", 10);
+
+    std::cout << gun << std::endl;
+    std::cout << beretta << std::endl;
+
+    beretta.reload();
+    beretta.prepare();
+
+    beretta.shoot();
+
+    std::cout << gun << std::endl;
+    std::cout << beretta << std::endl;
+    return 0;
+}
